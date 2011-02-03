b63dd1c92261d230a2f90a36ba4c9b0ef9b15eb1
Jonathan Smith <jonathan@impacti.org>
2011-02-03 11:08:21 +0000

    Added all the other files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travel::Application.config.secret_token = '5e383cd3c0154b859c64480d436f2e6965c5971e775d50a443fa6f67cd7e07ff3f31299d4242b78113a6c92b17266c3a17b4845e23afa6aede00b4824642721d'
